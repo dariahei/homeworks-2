cd10f5e9df187a9ad8287fa85098c14ba4dc983b
Daria Kuznetsova <daryakuznetsova@bk.ru>
2023-04-26 23:27:24 +0300

    event-object dropdown task var.2

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -4,33 +4,34 @@ let dropdownList = document.querySelectorAll('.dropdown__list');
 let dropdownValue = document.querySelectorAll('.dropdown__value');
 let dropdownItem = document.querySelectorAll('.dropdown__item');
 
-// const activeList = (event) => {
-//     console.log(event);
-//     console.log(event.currentTurget);
+const activeList = (event) => {
+    console.log(event);
+    console.log(event.currentTurget);
 
-//     event.target.nextElementSibling.classList.toggle('dropdown__list_active');
+    event.target.nextElementSibling.classList.toggle('dropdown__list_active');
     
-// }
-
-// dropdown.forEach (item =>  {
-//     item.addEventListener('click', activeList);
-// })
-
-for (let i = 0; i < dropdown.length; i++) {
-    dropdown[i].onclick = () => {
-        dropdownList[i].classList.toggle('dropdown__list_active');
-    }
 }
+
+dropdown.forEach (item =>  {
+    item.addEventListener('click', activeList);
+})
+
+// for (let i = 0; i < dropdown.length; i++) {
+//     dropdown[i].onclick = () => {
+//         dropdownList[i].classList.toggle('dropdown__list_active');
+//     }
+// }
 
 const newValue = (event) => {
     console.log(event.target.textContent);
     console.log(event.target.parentNode.parentNode);
     event.target.parentNode.parentNode.previousElementSibling.textContent = event.target.textContent;
-    event.target.parentNode.parentNode.classList.remove('dropdown__list_active');
+    event.target.parentNode.parentNode.classList.toggle('dropdown__list_active');
+    event.preventDefault();
 }
 
 dropdownItem.forEach (item => {
-    item.addEventListener('mousedown', newValue);
+    item.addEventListener('click', newValue);
 })
 
 
