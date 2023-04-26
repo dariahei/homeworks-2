526dc040351868b36984b5b1e0c50cdbce3c91fd
Daria Kuznetsova <daryakuznetsova@bk.ru>
2023-04-26 23:23:29 +0300

    event-object dropdown task

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,36 @@
+// let dropdownList = document.getElementsByClassName('dropdown__list');
+let dropdown = document.querySelectorAll('.dropdown');
+let dropdownList = document.querySelectorAll('.dropdown__list');
+let dropdownValue = document.querySelectorAll('.dropdown__value');
+let dropdownItem = document.querySelectorAll('.dropdown__item');
+
+// const activeList = (event) => {
+//     console.log(event);
+//     console.log(event.currentTurget);
+
+//     event.target.nextElementSibling.classList.toggle('dropdown__list_active');
+    
+// }
+
+// dropdown.forEach (item =>  {
+//     item.addEventListener('click', activeList);
+// })
+
+for (let i = 0; i < dropdown.length; i++) {
+    dropdown[i].onclick = () => {
+        dropdownList[i].classList.toggle('dropdown__list_active');
+    }
+}
+
+const newValue = (event) => {
+    console.log(event.target.textContent);
+    console.log(event.target.parentNode.parentNode);
+    event.target.parentNode.parentNode.previousElementSibling.textContent = event.target.textContent;
+    event.target.parentNode.parentNode.classList.remove('dropdown__list_active');
+}
+
+dropdownItem.forEach (item => {
+    item.addEventListener('mousedown', newValue);
+})
+
+
