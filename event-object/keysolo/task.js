29130017000a759055baab0c9e63fc69eea623bd
Daria Kuznetsova <daryakuznetsova@bk.ru>
2023-05-01 19:42:59 +0300

    event-obj\keysolo task

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,18 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    let lettersCompare = (event) => {
+      let programLetter =  (this.currentSymbol.textContent).toLowerCase();
+      let userLetter = (event.key).toLowerCase();
+
+      if (programLetter === userLetter) {
+        this.success()
+      } 
+      else {
+        this.fail();
+      }
+    }
+     document.addEventListener('keyup', lettersCompare);
   }
 
   success() {
